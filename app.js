776ba9cd6bececb889c8f63ddfcca9b8f4067431
moldovandeciroman <43988748+moldovandeciroman@users.noreply.github.com>
2019-11-20 16:48:14 +0200

    Add files via upload
    Dice Game Project with Front End

---
+++ app.js
@@ -0,0 +1,78 @@
+var scores, roundscores, activePlayer, dice;
+
+function init(){
+    scores = [0,0];
+    roundscores = 0;
+    activePlayer = 0;
+    dice = 0;
+    
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+init();
+document.querySelector('.btn-roll').addEventListener('click', function()                                                     
+{   dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+ dice + '.png';
+
+    if(dice !== 1)
+        {
+            roundscores += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundscores;
+        }
+ 
+ else
+     nextPlayer(); 
+                                                    })
+
+function nextPlayer()
+{
+    activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+    roundscores = 0;
+    
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-hold').addEventListener('click', function()
+                                                    {
+    
+    
+    scores[activePlayer] += roundscores;
+            document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    
+    if(scores[activePlayer] >= 100)
+        {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        }
+    else
+        nextPlayer();
+    
+    
+})
+
